fec8a881031d558665c0a16170e8a3aa8ef57bd0
Jacob Burch <jacob@burch.io>
2018-12-04 01:48:04 -0600

    dual monitor fixes for hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,15 +4,13 @@ hs.hotkey.bind({"cmd", "ctrl"}, "P", function()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = 0
-  f.y = 0
+  f.x = max.x
+  f.y = max.y
   f.w = max.w / 2
   f.h = max.h / 2
   win:setFrame(f, 0)
 end)
-hs.hotkey.bind({}, "f14", function()
-    hs.itunes.playpause()
-end)
+
 hs.hotkey.bind({"cmd", "ctrl"}, "[", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -25,6 +23,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "[", function()
   f.h = max.h / 2
   win:setFrame(f, 0)
 end)
+
 hs.hotkey.bind({"cmd", "ctrl"}, "]", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -37,6 +36,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "]", function()
   f.h = max.h / 2
   win:setFrame(f, 0)
 end)
+
 hs.hotkey.bind({"cmd", "ctrl"}, "'", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -49,6 +49,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "'", function()
   f.h = max.h
   win:setFrame(f, 0)
 end)
+
 hs.hotkey.bind({"cmd", "ctrl"}, "L", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -73,49 +74,57 @@ hs.hotkey.bind({"cmd", "ctrl"}, ".", function()
   f.h = max.h / 2
   win:setFrame(f, 0)
 end)
+
 hs.hotkey.bind({"ctrl"}, "Space", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = 0
-  f.y = 0
+  f.x = max.x
+  f.y = max.y
   f.w = max.w
   f.h = max.h
   win:setFrame(f, 0)
 end)
+
 hs.hotkey.bind({"cmd", "ctrl"}, ",", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = 0
+  f.x = max.x
   f.y = max.h / 2
   f.w = max.w / 2
   f.h = max.h / 2
   win:setFrame(f, 0)
 end)
+
 hs.hotkey.bind({"cmd", "ctrl"}, ".", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = 0
+  f.x = max.x
   f.y = max.h / 2
   f.w = max.w
   f.h = max.h / 2
   win:setFrame(f, 0)
 end)
+
 hs.hotkey.bind({"cmd", "ctrl"}, "/", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.w / 2
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h / 2
+  win:setFrame(f, 0)
   f.y = max.h / 2
   f.w = max.w / 2
   f.h = max.h / 2
